6e360caffac2de3d5852a5d8665fa112922c4826
Carla Gulizia <carla.gulizia@gmail.com>
2016-03-09 17:22:50 -0300

    Se modifico c.sum.c para llamarlo desde main de Fortran

--- multilenguaje/04-c-from-f/c-sum.c
+++ multilenguaje/04-c-from-f/c-sum.c
@@ -1,12 +1,13 @@
 #include <stdlib.h>
 #include "c-sum.h"
 
-int sum_abs(int *in, int num) {
+int sum_abs_(int *in, int *num, int *asum) {
    int i,sum;
 
-   for (i=0,sum=0; i < num; ++i) {
+   for (i=0,sum=0; i < *num; ++i) {
        sum += abs(in[i]);
    }
-   return sum;
+   *asum=sum;
+   return *asum;
 }
  
